f37944c88cc92088c9de341417fc846a6484c09e
Maxime Aubaret <maxime.aubaret@gmail.com>
2026-01-24 22:01:42 +0100

    Fix crash in clipboard handler for non-standard views
    Check if view is a ThunarStandardView before calling
    thunar_standard_view_queue_redraw(). Miller Columns view
    inherits from GtkScrolledWindow, not ThunarStandardView.

--- thunar/thunar-window.c
+++ thunar/thunar-window.c
@@ -2302,10 +2302,9 @@ thunar_window_clipboard_manager_changed (GtkWidget *widget)
 {
   ThunarWindow *window = THUNAR_WINDOW (widget);
 
-  /* check if the content actually can be pasted into thunar,
-   * in order to do not redraw the view if just some text is copied.
-   */
-  if (thunar_clipboard_manager_get_can_paste (window->clipboard) && G_LIKELY (window->view != NULL))
+  if (thunar_clipboard_manager_get_can_paste (window->clipboard)
+      && G_LIKELY (window->view != NULL)
+      && THUNAR_IS_STANDARD_VIEW (window->view))
     thunar_standard_view_queue_redraw (THUNAR_STANDARD_VIEW (window->view));
 }
 
